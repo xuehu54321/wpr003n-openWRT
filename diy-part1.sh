8945c4cb4c43f7e1a67849ccae0b1f7b35336dcd
Viking <77148401+zk26@users.noreply.github.com>
2024-08-20 20:43:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,6 +22,6 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 sudo timedatectl set-timezone "Asia/Shanghai"
 
 # Install the tools you need to use
-sudo apt update
-sudo apt-get install build-essential asciidoc binutils bzip2 gawk gettext git subversion libssl-dev libncurses5-dev patch unzip zlib1g-dev libc6-dev libbz2-dev gdisk flex python2
-git clone https://github.com/syb999/openwrt-15.05.git
+#sudo apt update
+#sudo apt-get install build-essential asciidoc binutils bzip2 gawk gettext git subversion libssl-dev libncurses5-dev patch unzip zlib1g-dev libc6-dev libbz2-dev gdisk flex python2
+#git clone https://github.com/syb999/openwrt-15.05.git
