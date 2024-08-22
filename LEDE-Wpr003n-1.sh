03dee0e5d7e9047426f2e90bb6ee499105827a6a
Viking <77148401+zk26@users.noreply.github.com>
2024-08-22 13:03:21 +0800

    Update LEDE-Wpr003n-1.sh

--- LEDE-Wpr003n-1.sh
+++ LEDE-Wpr003n-1.sh
@@ -22,6 +22,7 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 sudo timedatectl set-timezone "Asia/Shanghai"
 
 # Install the tools you need to use
-#sudo apt update
-#sudo apt-get install build-essential asciidoc binutils bzip2 gawk gettext git subversion libssl-dev libncurses5-dev patch unzip zlib1g-dev libc6-dev libbz2-dev gdisk flex python2
+sudo apt update
+sudo apt-get install build-essential asciidoc binutils bzip2 gawk gettext git subversion libssl-dev libncurses5-dev patch unzip zlib1g-dev libc6-dev libbz2-dev gdisk flex python2
 #git clone https://github.com/syb999/openwrt-15.05.git
+git clone https://github.com/openwrt/openwrt.git
